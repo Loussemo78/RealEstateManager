3ba6bb46ae7f7ce33fc3c84b685c0d1184893377
michael <lmfoundo@bouyguestelecom.fr>
2022-08-23 22:13:34 +0200

    repository in progress

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/repository/RealEstateRepository.kt
@@ -0,0 +1,6 @@
+package com.openclassrooms.realestatemanager.repository
+
+import com.openclassrooms.realestatemanager.dao.RealEstateDao
+
+class RealEstateRepository(private val realEstateDao: RealEstateDao) {
+}

bc01c66aa2092e55564e131902d5a93dc0b8fa92
michael <lmfoundo@bouyguestelecom.fr>
2022-09-11 18:44:31 +0200

    UI searchActivity created

--- app/src/main/java/com/openclassrooms/realestatemanager/SearchRealEstateActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/SearchRealEstateActivity.kt
@@ -1,11 +1,28 @@
 package com.openclassrooms.realestatemanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.openclassrooms.realestatemanager.databinding.ActivitySearchRealEstateBinding
+import com.openclassrooms.realestatemanager.views.RealEstateViewModel
+
 
 class SearchRealEstateActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchRealEstateBinding
+    private lateinit var viewModel: RealEstateViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_real_estate)
+        binding = ActivitySearchRealEstateBinding.inflate(layoutInflater)
+        val view: View = binding.root
+        setContentView(view)
+        viewModel = ViewModelProvider(this)[RealEstateViewModel::class.java]
+
+    }
+
+    private fun initializeSearchItems(){
+
     }
 }
